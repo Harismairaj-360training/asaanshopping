d4520687d1435d57108f960d87886e1abc8a9198
Haris Mairaj <haris.mairaj@360training.com>
2018-02-04 14:13:41 +0500

    Adding environment files as per the first deployed db

---
+++ app/etc/env.php
@@ -0,0 +1,60 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin123',
+  ),
+  'crypt' => 
+  array (
+    'key' => '0pavlig2jx7qdgytgkc6kchcrvmb5hul',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => 'mgcs_',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'pkcartde_mage812',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'production',
+  'session' => 
+  array (
+    'save' => 'db',
+  ),
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Fri, 15 Dec 2017 01:57:54 +0800',
+  ),
+);
