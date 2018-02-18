73cb73cde4a5c8f77252347b8a3d8f77dc50b30a
zaidhussainn <zaid_22@hotmail.com>
2018-02-18 18:02:56 +0500

    Product attributes create and attribute set with assignment. Return product collection on specific category and send data to front end in json format

--- app/code/AsaanShopping/SearchByLocation/Block/Search.php
+++ app/code/AsaanShopping/SearchByLocation/Block/Search.php
@@ -5,13 +5,60 @@ namespace AsaanShopping\SearchByLocation\Block;
 
 class Search extends \Magento\Framework\View\Element\Template
 {
+    protected $_productCollectionFactory;
+    protected $_categoryFactory;
+    protected $jsonHelper;
 
     /**
-     * @return string
+     * Constructor
+     *
+     * @param \Magento\Framework\Json\Helper\Data $jsonHelper
      */
-    public function googleapi()
+
+    public function __construct(
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+        \Magento\Catalog\Model\CategoryFactory $categoryFactory,
+        \Magento\Framework\Json\Helper\Data $jsonHelper,
+        \Magento\Framework\View\Element\Template\Context $context
+    )
     {
-        //Your block code
-        return 'Google API Method';
+        $this->_categoryFactory = $categoryFactory;
+        $this->_productCollectionFactory = $productCollectionFactory;
+        $this->jsonHelper = $jsonHelper;
+        parent::__construct($context);
+    }
+
+
+    public function getProductCollection()
+    {
+        $categoryId = '3';
+        $category = $this->_categoryFactory->create()->load($categoryId);
+        $collection = $this->_productCollectionFactory->create();
+        $collection->addAttributeToSelect('*');
+        //$collection->addCategoryFilter($category);
+        $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
+        $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
+        $productsData       =   $collection->getData();
+
+        try {
+            return $this->jsonHelper->jsonEncode($productsData);
+        } catch (\Magento\Framework\Exception\LocalizedException $e) {
+            return $this->jsonResponse($e->getMessage());
+        } catch (\Exception $e) {
+            $this->logger->critical($e);
+            return $this->jsonResponse($e->getMessage());
+        }
+    }
+
+    /**
+     * Create json response
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function jsonResponse($response = '')
+    {
+        return $this->getResponse()->representJson(
+            $this->jsonHelper->jsonEncode($response)
+        );
     }
 }
